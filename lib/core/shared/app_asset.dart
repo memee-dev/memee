c8cf1c448927080b142b6a977bcf13233df0a68d
memee-dev <memeeofficial23@gmail.com>
2023-12-01 00:25:54 +0530

    payment-anim

--- lib/core/shared/app_asset.dart
+++ lib/core/shared/app_asset.dart
@@ -3,4 +3,6 @@ mixin AppAsset {
   static const String carrot = 'asset/carrots.png';
   static const String chicken = 'asset/chicken.png';
   static const String beef = 'asset/beef.png';
+  static const String paymentSuccess = 'asset/anim/payment-success.json';
+  static const String paymentFailure = 'asset/anim/payment-failure.json';
 }
